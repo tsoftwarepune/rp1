e0a3b13caef25f222712c215196b628553e172c0
usedcarsatpune@gmail.com <usedcarsatpune@gmail.com>
2023-10-16 15:51:46 +0000

    Final Lineage

--- models/staging/OrderFact_Table.sql
+++ models/staging/OrderFact_Table.sql
@@ -7,9 +7,18 @@
 WITH OrderFact_Table AS 
 (
 select
-oc.*,ol.*
+ol.O_ORDERKEY,
+ol.O_CUSTKEY,
+ol.O_ORDERSTATUS,
+ol.O_TOTALPRICE,
+ol.O_ORDERDATE,
+ol.O_ORDERPRIORITY,
+ol.O_CLERK,
+ol.O_SHIPPRIORITY,
+ol.O_COMMENT,
+oc.C_NAME
 from {{ ref('Tops_order_Customer') }} as oc
 inner join {{ ref('Tops_order_lineitem') }} as ol
-on order_fact.o_orderkey = lineitem_dim.l_orderkey                                                    
+on oc.O_ORDERKEY = ol.O_ORDERKEY                                                    
 )
 select * from OrderFact_Table                                              

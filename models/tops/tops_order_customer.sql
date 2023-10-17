f56ae88a7657a4a47a0735a62c13abce29a16691
usedcarsatpune@gmail.com <usedcarsatpune@gmail.com>
2023-10-17 17:26:25 +0000

    Commit53

--- models/tops/tops_order_customer.sql
+++ models/tops/tops_order_customer.sql
@@ -9,7 +9,7 @@ WITH order_Customer AS
 (
 select
 order_fact.*,Customer_dim.c_name 
-from {{ ref('tops_order_model') }} as order_fact
+from {{ ref('tgen_order_model') }} as order_fact
 inner join {{ ref('tgen_customer_model') }} as Customer_dim
 on order_fact.o_custkey = Customer_dim.c_custkey
 )
--- models/tops/tops_order_lineitem.sql
+++ models/tops/tops_order_lineitem.sql
@@ -9,7 +9,7 @@ WITH order_lineitem AS
 (
 select
 order_fact.*,lineitem_dim.*
-from {{ ref('tops_order_model') }} as order_fact
+from {{ ref('tgen_order_model') }} as order_fact
 inner join {{ ref('tgen_lineitem_model') }} as lineitem_dim
 on order_fact.o_orderkey = lineitem_dim.l_orderkey                                                    
 )
